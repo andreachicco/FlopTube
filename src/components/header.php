503bb1fd6cc862c56a0d7e118d019bd807bf43d9
is_andreachicco <s5128817@studenti.unige.it>
2023-12-15 16:40:37 +0100

    logo and home link redirect to home page (index.php)

--- src/components/header.php
+++ src/components/header.php
@@ -2,7 +2,7 @@
     <div class="wrapper w-sm-container sm:w-container w-4/5 mx-auto h-full">
         <nav class="flex items-center justify-between h-full">
             <div id="logo-container" class="h-full">
-                <a href="#" class="h-full">
+                <a href="/" class="h-full">
                     <img class="min-w-[6rem] w-1/2 h-full" src="/assets/floptube_complete_logo.svg" alt="FlopTube Logo" />
                 </a>
             </div>
@@ -11,11 +11,11 @@
                 <div class="origin-left rounded line second w-burger-line-w h-burger-line-h bg-black m-1"></div>
                 <div class="origin-left rounded line third w-burger-line-w h-burger-line-h bg-black m-1"></div>
             </div>
-            <ul class="links bg-white absolute justify-around h-1/2 w-screen top-0 left-[100vw] flex flex-col sm:flex-row sm:bg-transparent sm:static sm:justify-end items-center list-none">
-                <li class="text-sm hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">Home</a></li>
-                <li class="text-sm hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">Videos</a></li>
-                <li class="text-sm hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">SignIn</a></li>
-                <li class="text-sm hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">SignUp</a></li>
+            <ul class="uppercase text-sm links bg-white absolute justify-around h-1/2 w-screen top-0 left-[100vw] flex flex-col sm:flex-row sm:bg-transparent sm:static sm:justify-end items-center list-none">
+                <li class="hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="/">Home</a></li>
+                <li class="hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">Videos</a></li>
+                <li class="hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">SignIn</a></li>
+                <li class="hover:text-ft-red p-sm-nav-links md:p-md-nav-links lg:p-lg-nav-links"><a href="#">SignUp</a></li>
             </ul>
         </nav>
     </div>

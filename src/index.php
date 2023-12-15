32a0339015b1a2baa86bf292f3aec13b7d018d31
is_andreachicco <s5128817@studenti.unige.it>
2023-12-15 10:25:35 +0100

    project setup

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html class="overflow-x-hidden" lang="en">
+<head>
+    <?php require_once(dirname(__FILE__) . "/common/head.php"); ?>
+    <title>FlopTube</title>
+</head>
+<body class="overflow-x-hidden">
+    <h1 class="text-ft-red">FlopTube: like YouTube, but worse</h1>
+</body>
+</html>
